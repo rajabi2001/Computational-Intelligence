dc4710147788c471f9ac271e802c64cb08bae9e4
rajabi2001 <mj.rajabi2001@gmail.com>
2022-06-27 14:07:38 +0430

    evolutionary game part 1

--- Evolutionary-Projects/evolution.py
+++ Evolutionary-Projects/evolution.py
@@ -1,11 +1,93 @@
 import copy
-
+import math
+import numpy as np
 from player import Player
 
 
 class Evolution:
     def __init__(self):
         self.game_mode = "Neuroevolution"
+        # options = q_tournament , roulette_wheel , sus
+        self.selection_mode = "q_tournament"
+
+    def q_tournament(self, num_players, players, q ):
+        selected_players = []
+        for i in range(num_players) :
+             random_selections = np.random.choice(players, q)
+             selected_players.append(max(random_selections, key=lambda player: player.fitness))
+        return selected_players
+
+    def roulette_wheel(self , players , num_player):
+        fitness_sum = sum([player.fitness for player in players])
+        probabilities = [player.fitness / fitness_sum for player in players]
+        next_generation = np.random.choice(players, size=num_player, p=probabilities, replace=False)
+        return next_generation.tolist()
+
+    def sus(self, players, num_players):
+        total_fitness = np.sum([p.fitness for p in players])
+        step_size = total_fitness / num_players
+        start_point = np.random.uniform(0, step_size)
+
+        ruler = np.arange(num_players) * step_size
+        ruler = ruler + start_point
+
+        selected_players = []
+        for r in ruler:
+            i = 0
+            f = 0
+            while f < r:
+                f += players[i].fitness
+                i += 1
+            selected_players.append(players[i - 1])
+        return selected_players
+
+
+    def crossover(self, player1, player2):
+
+        crossover_probability = 0.8
+        random_number = np.random.uniform(0, 1, 1)
+        if(random_number >= crossover_probability) :
+            return player1 , player2
+
+        child1 = self.clone_player(player1)
+        child2 = self.clone_player(player2)
+
+        crossover_place1 = math.floor(player1.nn.hidden_size[1] / 2)
+        crossover_place2 = math.floor(player1.nn.output_size[2] / 2)
+
+        child1.nn.w1 = np.concatenate((player1.nn.w1[:crossover_place1], player2.nn.w1[crossover_place1:]), axis=0)
+        child1.nn.b1 = np.concatenate((player1.nn.b1[:crossover_place1], player2.nn.b1[crossover_place1:]), axis=0)
+        child1.nn.w2 = np.concatenate((player1.nn.w2[:crossover_place2], player2.nn.w2[crossover_place2:]), axis=0)
+        child1.nn.b2 = np.concatenate((player1.nn.b2[:crossover_place2], player2.nn.b2[crossover_place2:]), axis=0)
+        child2.nn.w1 = np.concatenate((player2.nn.w1[:crossover_place1], player1.nn.w1[crossover_place1:]), axis=0)
+        child2.nn.b1 = np.concatenate((player2.nn.b1[:crossover_place1], player1.nn.b1[crossover_place1:]), axis=0)
+        child2.nn.w2 = np.concatenate((player2.nn.w2[:crossover_place2], player1.nn.w2[crossover_place2:]), axis=0)
+        child2.nn.b2 = np.concatenate((player2.nn.b2[:crossover_place2], player1.nn.b2[crossover_place2:]), axis=0)
+
+        return child1,child2
+
+    def mutation(self, player):
+        mutation_probability = 0.3
+
+        layer_sizes = player.nn.layer_sizes
+        if np.random.uniform(0, 1, 1) < mutation_probability:
+            player.nn.w1 += np.random.randn(layer_sizes[1], layer_sizes[0])
+        if np.random.uniform(0, 1, 1) < mutation_probability:
+            player.nn.w2 += np.random.randn(layer_sizes[2], layer_sizes[1])
+        if np.random.uniform(0, 1, 1) < mutation_probability:
+            player.nn.b1 += np.random.randn(layer_sizes[1], 1)
+        if np.random.uniform(0, 1, 1) < mutation_probability:
+            player.nn.b2 += np.random.randn(layer_sizes[2], 1)
+  
+
+        # random_number = np.random.uniform(0, 1, 1)
+        # if(random_number <= mutation_probability) :
+        #     player.nn.w1 += np.random.normal(0,0.3,size=player.nn.w1.shape)
+        #     player.nn.w2 += np.random.normal(0,0.3,size=player.nn.w2.shape)
+        #     player.nn.b1 += np.random.normal(0, 0.3, size=player.nn.b1.shape)
+        #     player.nn.b2 += np.random.normal(0, 0.3, size=player.nn.b2.shape)
+
+        return player
 
     def next_population_selection(self, players, num_players):
         """
@@ -34,9 +116,29 @@ class Evolution:
         if first_generation:
             return [Player(self.game_mode) for _ in range(num_players)]
         else:
-            # TODO ( Parent selection and child generation )
-            new_players = prev_players  # DELETE THIS AFTER YOUR IMPLEMENTATION
-            return new_players
+
+            if self.selection_mode == "q_tournament":
+                new_players = self.q_tournament(num_players, prev_players, q=3)
+            elif self.selection_mode == "roulette_wheel":
+                new_players = self.roulette_wheel(prev_players, num_players)
+            elif self.selection_mode == "sus":
+                new_players = self.sus(prev_players, num_players)
+
+            children = []
+            for i in range( 0, len(new_players) , 2) :
+                parent1 = new_players[i]
+                parent2 = new_players[i+1]
+                child1, child2 = self.crossover(parent1,parent2)
+                child1 = self.mutation(child1)
+                child2 = self.mutation(child2)
+                children.append(child1)
+                children.append(child2)
+
+            return children
+
+
+            
+            
 
     def clone_player(self, player):
         """
